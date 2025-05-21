77ed10649ce61f78b292a85cb4779e690c3d1676
Diegosm10 <diego666.segovia777@gmail.com>
2025-05-21 16:31:35 -0300

    Breeze instalado

--- SistemaVial/routes/web.php
+++ SistemaVial/routes/web.php
@@ -1,24 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ConstructionController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\MachineController;
 use App\Http\Controllers\ConstructionMachinesController;
 use App\Http\Controllers\MaintenanceController;
-use App\Models\Construction;
-use App\Models\Machine;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/machines', MachineController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/constructions', ConstructionController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/constructions', ConstructionController::class);
+    Route::resource('/machines', MachineController::class);
+    Route::resource('/constructionMachines', ConstructionMachinesController::class);
+    Route::resource('/maintenance', MaintenanceController::class);
+});
 
-Route::resource('/constructionMachines', ConstructionMachinesController::class);
-
-Route::resource('/maintenance', MaintenanceController::class);
-
-
+require __DIR__.'/auth.php';
